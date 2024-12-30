e05e78dae95739c4531b15ca7bdef018ea520d0b
Johnny li <johnnyli@Johnnys-Laptop.local>
2024-12-29 22:27:12 -0500

    Update: Cleaned Up the recipe page

--- app/recipe/RecommendRecipes.tsx
+++ app/recipe/RecommendRecipes.tsx
@@ -1,8 +1,6 @@
 import { getRecommendRecipes } from "@/fetchData";
 import Link from "next/link";
-import AddCartButton from "@/components/AddCartButton";
 import Image from "next/image";
-import { ArrowLongRightIcon } from '@heroicons/react/24/solid'
 import Recipe from "@/types/recipeType";
 
 import {
@@ -15,15 +13,17 @@ import {
 
 type Props = {
   area: string;
+  id: string;
 };
 
-const RecommendRecipes = async ({ area }: Props) => {
+const RecommendRecipes = async ({ area, id }: Props) => {
   const { meals: recommendRecipes } = await getRecommendRecipes(area);
+  const filtered = recommendRecipes.filter((recipe: Recipe) => recipe.idMeal !== id)
 
   return (
     <div className="mt-3 mb-10">
       <h2 className="text-4xl text-blue-500 dark:text-gray-500 tracking-widest mb-4">
-        Recommended Recipes : {recommendRecipes?.length}
+        Recommended Recipes : {filtered.length}
       </h2>
 
       <Carousel
@@ -33,12 +33,8 @@ const RecommendRecipes = async ({ area }: Props) => {
         }}
         className='w-full mx-auto lg:w-full'
       >
-        <div className="lg:hidden flex items-center mx-auto justify-center">
-          <h2>Swipe</h2>
-          <ArrowLongRightIcon className="w-7 ml-2"/>
-        </div>
         <CarouselContent className="">
-          {recommendRecipes && recommendRecipes.map((recommend: Recipe) => (
+          {filtered.map((recommend: Recipe) => (
             <CarouselItem
               key={recommend.idMeal}
               className="basis-1/2 md:basis-1/3 lg:basis-1/4 2xl:basis-1/6 mx-auto"
